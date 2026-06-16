a558fa8df08094c9883da281d4979dab3997d257
Zoeytothemoon <wishcode.studio@gmail.com>
2026-06-16 10:23:56 +0700

    feat: Implement taskModule - implement taskModule which is declare controller, service and export

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [],
+  imports: [TasksModule],
   controllers: [AppController],
   providers: [],
 })
